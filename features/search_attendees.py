da4e2a0edeca49012b2e9607b84d55b618a681d5
Muhammad Zulqarnain <muhammad.znain@gmail.com>
2026-09-07 13:55:34 +0100

    Add attendee search

--- features/search_attendees.py
+++ features/search_attendees.py
@@ -1,9 +1,20 @@
-for attendee in attendees:
-    if search_name in attendee["name"].lower():
-        print("Attendee found!")
-        print("Name:", attendee["name"])
-        print("Event:", attendee["event"])
-        attendee_found = True
+    if choice == 4:
+        search_name = input(
+            "Enter the attendee's name: "
+        ).strip().lower()
 
-if attendee_found == False:
-    print("Attendee not found.")
+        attendee_found = False
+
+        for attendee_id, attendee_name in names_dict.items():
+            if search_name in attendee_name.lower():
+
+                for event in events_dict.values():
+                    if attendee_id in event["attendee_ids"]:
+                        print("\nAttendee found!")
+                        print("Name:", attendee_name)
+                        print("Event:", event["event_name"].title())
+                        attendee_found = True
+
+        if attendee_found == False:
+            print("Attendee not found.")
+
