f9c833172f3e4752c5bbc8b2351045e968cf0b04
Muhammad Zulqarnain <muhammad.znain@gmail.com>
2026-09-07 12:01:53 +0100

    Add attendee search

---
+++ features/search_attendees.py
@@ -0,0 +1,9 @@
+for attendee in attendees:
+    if search_name in attendee["name"].lower():
+        print("Attendee found!")
+        print("Name:", attendee["name"])
+        print("Event:", attendee["event"])
+        attendee_found = True
+
+if attendee_found == False:
+    print("Attendee not found.")
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-## View_available_events
 
-print("hello world")
+
+
+
+        for attendee in attendees:
+            if search_name in attendee["name"].lower():
+                print("Attendee found!")
+                print("Name:", attendee["name"])
+                print("Event:", attendee["event"])
+                attendee_found = True
+
+        if attendee_found == False:
+            print("Attendee not found.")
+
